24a7758e947d8c0967098e2eeae07b9699a4c2eb
SashnikovNA <kapodastr@inbox.ru>
2022-11-11 01:22:30 +0300

    withHTML

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 
-//1st message
+
 const Keyboard_1 = [[
     { text: 'Приступаем!', callback_data: 'go_to_2' },
     Markup.button.url("По ссылке", "https://covid19.who.int/") 
@@ -15,15 +15,30 @@ const Keyboard_1 = [[
 const Keyboard_2 = [[
         { text: 'Подключаем к теории практику - рутина месяца', callback_data: 'go_to_3' }]];
     
-//const m1 = `Приветствую Вас , ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'} , в Safety Zone!`;
 
-bot.start((ctx) => {
-    ctx.sendMessage(posts.post1, { reply_markup: JSON.stringify({ inline_keyboard: Keyboard_1 }), parse_mode: 'MarkdownV2' });
-});
+const Keyboard_3 = [[
+    { text: 'Рутина месяца (практика) - суть', callback_data: 'go_to_4' }]];
 
+
+
+//message 1
+bot.start(
+    (ctx) => {
+        ctx.replyWithHTML(posts.post1, {reply_markup: JSON.stringify({ inline_keyboard: Keyboard_1 })});
+    }
+);
+
+//message 2
 bot.action('go_to_2', (ctx) => {
     ctx.replyWithVoice({ source: './SZ_resource_3.1.m4a'}).then(() => {
-        ctx.sendMessage(posts.post2, { reply_markup: JSON.stringify({ inline_keyboard: Keyboard_2 }), parse_mode: 'MarkdownV2' });
+        ctx.replyWithHTML(posts.post2, {reply_markup: JSON.stringify({ inline_keyboard: Keyboard_2 })});
+    });
+} );
+
+//message 3
+bot.action('go_to_3', (ctx) => {
+    ctx.replyWithVoice({ source: './SZ_resource_#3.2_результат.m4a'}).then(() => {
+        ctx.replyWithHTML(posts.post3, {reply_markup: JSON.stringify({ inline_keyboard: Keyboard_3 })});
     });
 } );
 
